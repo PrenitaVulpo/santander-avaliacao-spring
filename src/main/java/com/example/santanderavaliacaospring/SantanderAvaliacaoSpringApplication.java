0d03404a99e93e11b4b472a620718e5a7ecfba79
PrenitaVulpo <guilhermepdl97@protonmail.com>
2022-03-16 15:04:10 -0300

    rework

--- src/main/java/com/example/santanderavaliacaospring/SantanderAvaliacaoSpringApplication.java
+++ src/main/java/com/example/santanderavaliacaospring/SantanderAvaliacaoSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SantanderAvaliacaoSpringApplication {
-
+	public static Resistance resistance = new Resistance();
 	public static void main(String[] args) {
 		SpringApplication.run(SantanderAvaliacaoSpringApplication.class, args);
 	}
